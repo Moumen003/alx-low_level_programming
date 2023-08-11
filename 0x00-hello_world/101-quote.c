4b56160711f287f96e7fffe43bf8e2b60d133fdb
Moumen003 <moumenibrahim219@yahoo.com>
2023-08-11 04:56:12 +0300

    test

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+/**
+ * main - start
+ *
+ * Description: test
+ *
+ * Return: bs
+*/
+
+int main(void)
+{
+const char *sentence = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(1, sentence, sizeof(sentence - 1);
+return (1);
+}
