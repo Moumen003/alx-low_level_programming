e0a6ff618fab76afa2db781154018d18e710a10b
Moumen003 <moumenibrahim219@yahoo.com>
2023-08-15 00:17:49 +0300

    this is start

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: bengarab
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+int i;
+for (i = 48; i <= 102; i++)
+{
+if (i == 57)
+{
+i += 39;
+}
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
