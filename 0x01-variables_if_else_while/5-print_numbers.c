6fb7d074719e0dd5facb67134ae07b9541e71b74
Moumen003 <moumenibrahim219@yahoo.com>
2023-08-14 23:53:57 +0300

    this is start

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: bengarab
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+printf("%d", i);
+}
+putchar('\n');
+return (0);
+}
