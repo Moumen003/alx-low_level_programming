9d3df1acc5f6c0278fc0d3d7037a462484be3906
Moumen003 <moumenibrahim219@yahoo.com>
2023-08-15 00:02:15 +0300

    this is start

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: bengarab
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+int ch;
+for (ch = 'z'; ch >= 'a'; ch--)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
