98278736b265da2588ab5e029611c4d5cb1a5afb
Moumen003 <moumenibrahim219@yahoo.com>
2023-08-14 23:42:11 +0300

    this is start

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: bengarab
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+char ch;
+ch = 'a';
+while (ch <= 'z')
+{
+putchar(ch);
+ch++;
+}
+ch = 'A'
+while (ch <= 'Z')
+{
+putchar(ch);
+ch++;
+}
+putchar('\n')
+return (0);
+}
