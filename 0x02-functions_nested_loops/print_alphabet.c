3c37eadf991079639ce2b408ae6a045984b95dc6
Moumen003 <moumenibrahim219@yahoo.com>
2023-08-15 23:45:25 +0300

    test

---
+++ 0x02-functions_nested_loops/print_alphabet.c
@@ -0,0 +1,18 @@
+"include main.h"
+
+/**
+ * print_alphabet - write
+ *
+ * Return: 0 (success)
+*/
+
+void print_alphabet(void)
+{
+int ch = 'a';
+while (ch <= 'z')
+{
+_putchar(ch);
+ch++;
+}
+_putchar('\0');
+}
