ff65b977c9c41de0fe78ed74f165b6446661cd0a
Moumen003 <moumenibrahim219@yahoo.com>
2023-08-16 00:03:26 +0300

    tr

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10: use
+*/
+
+void print_alphabet_x10(void)
+{
+int i;
+int ch;
+for (i = 0; i < 10; i++)
+{
+ch = 'a'
+while (ch <= 'z')
+{
+_putchar(ch);
+ch++
+}
+_putchar('\n');
+}
+return (0)
+}
