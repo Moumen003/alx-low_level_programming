f3ae6f4a1f875978e046e7103fa6571fa5da7edb
Moumen003 <moumenibrahim219@yahoo.com>
2023-08-16 01:58:34 +0300

    bs

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_to_98 - name of function
+ * @n: int
+*/
+
+void print_to_98(int n)
+{
+int c;
+
+if (n > 98)
+{
+	for (c == n; c >= 98;  c--)
+	{
+		printf("%d, ", c);
+	}
+}
+else
+{
+	for (c == n; c <= 98; c++)
+	{
+		printf("%d, ", c);
+	}
+}
+printf("98\n");
+}
