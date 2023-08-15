d06e1c0a5c470eb15ed50d6fa48a764dfe4cb857
Moumen003 <moumenibrahim219@yahoo.com>
2023-08-15 23:08:40 +0300

    trail

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+
+/**
+ * main - entry point
+ *
+ * Description: bs keda
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+char a[0] = '_', a[1] = 'p', a[2] = 'u';
+char a[3] = 't', a[4] = 'c', a[5] = 'h';
+char a[6] = 'a', a[7] = 'r';
+int i = 0;
+while (a[i] != 0)
+{
+_putchar(a[i]);
+i++;
+}
+return (0);
+}
