d786ebd5cb9ba3ab2c6f4a7f8a385f06465d4ac5
Moumen003 <moumenibrahim219@yahoo.com>
2023-08-16 01:14:22 +0300

    ben

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,12 +9,13 @@ void jack_bauer(void)
 int h, m;
 for (h = 0; h <= 23; h++)
 {
-	if (h <= 9)
-	{
-		_putchar('0');
-	}
 	for (m = 0; m <= 59; m++)
 	{
+		if (h <= 9)
+        {
+                _putchar('0');
+        }
+
 		_putchar(h + '0');
 		_putchar(':');
 		if (m <= 9)
@@ -23,7 +24,6 @@ for (h = 0; h <= 23; h++)
 		}
 		_putchar(m + '0');
 		_putchar('\n');
-	}
-	
+	}	
 }
 }
