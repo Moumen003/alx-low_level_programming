0b09180137f0f3f960607699b924619dea5b6d3d
Moumen003 <moumenibrahim219@yahoo.com>
2023-08-16 01:59:14 +0300

    bs

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,14 +11,14 @@ int c;
 
 if (n > 98)
 {
-	for (c == n; c >= 98;  c--)
+	for (c = n; c >= 98;  c--)
 	{
 		printf("%d, ", c);
 	}
 }
 else
 {
-	for (c == n; c <= 98; c++)
+	for (c = n; c <= 98; c++)
 	{
 		printf("%d, ", c);
 	}
