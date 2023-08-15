6ff907d1d2b8b665705cab13e7912a3b16875e94
Moumen003 <moumenibrahim219@yahoo.com>
2023-08-16 01:32:59 +0300

    yarab

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * times_table - function
+*/
+
+void times_table(void)
+{
+int i, x;
+for (i = 0; i < 10; i++)
+{
+	for (x = 0; x < 10; x++)
+	{
+		if (x != 0)
+		{
+			_putchar(' ');
+		}
+		if (i * x < 10)
+		{
+			_putchar(i * x + 48);
+		}
+		else
+		{
+			_putchar(i * x / 10 + 48);
+			_putchar(i * x % 10 + 48);
+		}
+		if (x != 9)
+		{
+			_putchar(',');
+		}
+	}
+}
+}
