ca500954c3abc3c0166be553ded3bc074ee026cb
Moumen003 <moumenibrahim219@yahoo.com>
2023-08-16 00:49:34 +0300

    garab

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_last_digit - function name
+ * @n: print last
+ *
+ * Return: hanshoof
+*/
+
+int print_last_digit(int n)
+{
+int lg;
+if (n >= 0)
+{
+lg = n % 10;
+}
+else
+{
+lg = ((n % 10) * -1);
+}
+_putchar('lg');
+return (lg);
+}
