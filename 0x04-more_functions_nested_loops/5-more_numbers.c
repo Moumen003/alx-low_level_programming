1b37d15aa676df107f6e5c68877be5ef77af6ed7
Moumen003 <moumenibrahim219@yahoo.com>
2023-08-17 23:55:21 +0300

    try

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - function
+ */
+
+void more_numbers(void)
+{
+	int j, k;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (k = 0; i <= 14; i++)
+		{
+			if (k <= 9)
+				_putchar(i);
+			else
+			{
+				_putchar('1');
+				_putchar(i % 10);
+			}
+		}
+		k = 0;
+	}
