0cce8537444bada03c2c0009ef2a65a8e9fd111f
Moumen003 <moumenibrahim219@yahoo.com>
2023-08-17 21:53:16 +0300

    bagarab

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.c"
+
+/**
+ * _isupper - function name
+ * @c: input
+ *
+ * Return: number
+*/
+
+int _isupper(int c)
+{
+	if (c >= 45 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
