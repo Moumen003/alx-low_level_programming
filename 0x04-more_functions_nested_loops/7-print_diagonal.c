dee5443d744d1d5d53db032f53816baa327f7970
Moumen003 <moumenibrahim219@yahoo.com>
2023-08-18 01:08:31 +0300

    try

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,19 @@
+#include "main.c"
+
+/**
+ * print_diagonal - function name
+ * @n: input
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+			_putchar(' ');
+		_putchar('\\');
+	}
+	_putchar('\n');
+}
