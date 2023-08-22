f35bf8a6b3c271b78635047aed6e85d4ec20506b
Moumen003 <moumenibrahim219@yahoo.com>
2023-08-22 20:24:26 +0300

    el7

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * keygen - file name
+ *
+ * Description: gets the password for a file
+ *
+ * Return: there should be no return
+ */
+
+int main(void)
+{
+	int m = srand(time(NULL));
+
+	printf("Tada! Congrats\n");
+	return (m);
