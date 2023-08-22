3cb5c98baa1037ec1e0b353ecc2167dc7123abd3
Moumen003 <moumenibrahim219@yahoo.com>
2023-08-22 05:48:13 +0300

    el7

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,6 +8,14 @@
 
 void print_array(int *a, int n)
 {
-	while (n--)
-		printf("%p, \n",*a[i]);
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i != (n-1))
+			printf("%d, ", a[i]);
+		else
+			printf("%d", a[i]);
+	}
+	printf("\n");
 }
