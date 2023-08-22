7abb9af6a78fb79bff9565646406089c9bcfff7e
Moumen003 <moumenibrahim219@yahoo.com>
2023-08-22 05:27:00 +0300

    el7

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,12 +7,13 @@
 
 void puts2(char *str)
 {
-	int i = 0;
+	int i;
 
-	while (str[i])
-		i++;
-	for (i = 0; str[i] != '\0'; i += 2)
-		_putchar(str[i]);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
 	_putchar('\n');
 }
 
