868c7f726e55e6985b525e80860f9f2affba413d
Moumen003 <moumenibrahim219@yahoo.com>
2023-08-22 04:35:22 +0300

    el7

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - function name
+ * @s: input
+ *
+ * Return : output of type int
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	return (i);
+}
