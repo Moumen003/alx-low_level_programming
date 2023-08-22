90348358266caf37f9e3c020591de5e70f96e3a4
Moumen003 <moumenibrahim219@yahoo.com>
2023-08-22 05:34:10 +0300

    el7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/** puts_half - fn name
+ * @str: bs ya input
+ */
+
+void puts_half(char *str)
+{
+	int i = 0;
+
+	while(str[i])
+		i++;
+	if (i % 2 == )0
+		i /= 2;
+	else
+		i = (i - 1) / 2;
+	for (; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
