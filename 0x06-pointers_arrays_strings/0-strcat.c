cf94990a6b558032b7de1709656fcee04e70bbaa
Moumen003 <moumenibrahim219@yahoo.com>
2023-08-24 19:19:03 +0300

    self

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,7 +3,7 @@
 /** 
  *_strcat - Concatenates two strings 
  * @dest: Destination string 
- * * @src: Source string 
+ * @src: Source string 
  * 
  * Return: Pointer to the resulting string (dest) 
  */ 
@@ -11,15 +11,16 @@
 char *_strcat(char *dest, const char *src) 
 { 
 	int i = 0; 
-	int j = 0; 
-	while (dest[i] != '\0') 
-		i++; 
-	while (src[j] != '\0') 
-	{ 
-		dest[i] = src[j]; 
-		i++; 
-		j++; 
+	int j = 0;
+
+	while (dest[i]) 
+		i++;
+
+	for  (j = 0; src[j]; j++)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
-	dest[i] = '\0';
 	return (dest);
 }
