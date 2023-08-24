c919f7ea909a0696d4bb535366f1ae5b840faa08
Moumen003 <moumenibrahim219@yahoo.com>
2023-08-24 21:05:22 +0300

    self

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcpy - function name
+ * @dest: first
+ * @src: second
+ * @n: third
+ *
+ * Return: the char output
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, c;
+
+	c = 0;
+	while (dest[c])
+		c++;
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i + c] == src[i];
+	}
+	dest[c + i] = '\0';
+	return (dest);
+}
