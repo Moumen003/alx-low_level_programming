3f9b6ee6926a56af08102c900d2e4f3b5d192c0f
Moumen003 <moumenibrahim219@yahoo.com>
2023-08-24 21:37:26 +0300

    self

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strncpy - funtion name
+ *
+ * @dest: first
+ * @src: take from
+ * @n: how many
+ *
+ * Return: el yegy
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
