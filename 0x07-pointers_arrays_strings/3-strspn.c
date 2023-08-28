de7710a880d51650ba4ec1ed94af097b1f281ba6
Moumen003 <moumenibrahim219@yahoo.com>
2023-08-28 22:40:09 +0000

    include

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -16,10 +16,10 @@ unsigned int j;
 for (i = 0; s[i] != '\0'; i++)
 {
 	for (j = 0; accept[j] != s[i]; j++)
-		{
-			if (accept[j] == '\0')
-				return (i);
-		}
+	{
+		if (accept[j] == '\0')
+			return (i);
+	}
 }
 return (i);
 }
