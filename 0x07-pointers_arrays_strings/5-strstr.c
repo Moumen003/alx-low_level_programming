fa2d9cb454f266861ae6d71ac222b9bc04175e15
Moumen003 <moumenibrahim219@yahoo.com>
2023-08-29 00:19:46 +0000

    include

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+*_strstr - bs ab2a
+*@haystack: bs ba2a
+*@needle: we malo
+*
+*Return: 2ooly keda
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+int i;
+int j;
+int p;
+
+p = 0;
+for (i = 0; haystack[i] != '\0'; i++)
+{
+	j = 0;
+	while (haystack[i] == needle[j])
+	{
+		j++;
+		if (needle[j] == '\0')
+			break;
+			p = 1;
+	}
+	if (p == 1)
+		break;
+}
+return (haystack + i - j);
+}
