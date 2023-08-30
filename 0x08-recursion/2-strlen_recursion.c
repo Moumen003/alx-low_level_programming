8d2f595212994897e23f1c56f3936e63d9f92ca3
Moumen003 <moumenibrahim219@yahoo.com>
2023-08-31 02:32:57 +0300

    mayala

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - funciton name even look recursion
+ * @s: bos keda
+ *
+ * Return: i
+ */
+
+int _strlen_recursion(char *s)
+{
+	int i;
+
+	i = 0;
+	if (*s >= '\0')
+		i += _strlen_recursion(s + 1) + 1;
+	return (i);
+}
