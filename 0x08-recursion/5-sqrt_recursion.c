d76296f5ea8cb2f31c5f28d0c47661c4d665cb47
Moumen003 <moumenibrahim219@yahoo.com>
2023-08-31 02:55:47 +0300

    mayala

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#inlcude "main.h"
+
+/**
+ * _sqrt_recursion - fn name
+ * @n: input
+ * @val: check
+ * Return: output
+ */
+
+
+int s(int n, int val);
+int _sqrt_recursion(int n)
+{
+	return (s(n, 1));
+}
+
+
+/**
+ * s - main
+ * @n: bos
+ * @val: always s=check
+ *
+ * Return: 8aha
+ */
+
+int s(int n, int val)
+{
+	if (val * val == n)
+		return (n);
+	else if (val * val < n)
+		return (square(n, val + 1));
+	else
+		return (-1);
+}
