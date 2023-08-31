be96ce6d2bee8b77cf56442fabe1150163e67652
Moumen003 <moumenibrahim219@yahoo.com>
2023-08-31 03:13:32 +0300

    mayala

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * is_prime_number - fn name
+ * @n: input
+ * @o: bos keda
+ * Return: shoof keda
+ */
+
+int cp(int n, int o);
+int is_prime_number(int n)
+{
+	return (cp(n, 2));
+}
+
+/**
+ * cp - name
+ * @n: shdfv
+ * @o: happ
+ * Return: shoof el feha
+ */
+
+int cp(int n, o)
+{
+	if (o >= n && n > 1)
+		return (1);
+	else if (n % o == 0 || n <= 1)
+		return (0);
+	else
+		return (cp(n, o + 1));
+}
