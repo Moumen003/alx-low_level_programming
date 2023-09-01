20a6b112ba96ca6cf092b1f244157c7bf117e681
“Moumen003” <moumenibrahim219@yahoo.com>
2023-09-01 18:39:06 +0000

    what

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - fn name
+* @argc: int
+* @argv: list
+* Return: 0
+*/
+
+
+
+int main(int argc, char const *argv[])
+{
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
+}
