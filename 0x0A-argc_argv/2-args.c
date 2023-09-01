e0ecf3ec15731592cd7c49f256431882f8878017
“Moumen003” <moumenibrahim219@yahoo.com>
2023-09-01 18:51:02 +0000

    what

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - fn name
+* @argc: hanshoof
+* @argv: elfeha
+* Return: teslm
+*/
+
+int main(int argc, char *argv[])
+{
+	int i;
+	for (i = 0; i <= argc; i++)
+	{
+		printf("%s\n", argv[argc]);
+	}
+}
