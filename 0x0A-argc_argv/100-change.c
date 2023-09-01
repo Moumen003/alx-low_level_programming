c344bc2ac0d26e83cb79c3b7882bd02c2d202e56
“Moumen003” <moumenibrahim219@yahoo.com>
2023-09-01 20:25:45 +0000

    what

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - fn name
+* @argc: hanshoof
+* @argv: elfeha
+* Return: teslm
+*/
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+		return (printf("Error\n"), 1);
+	else
+	{
+		int i;
+		int lc;
+ 		int m;
+		
+		int c[] = {25, 10, 5, 2, 1};
+		lc = 0;
+		m = atoi(argv[1]);
+
+		for (i = 0; i < 5; i++)
+		{
+			if (m > c[i])
+			{
+				lc += m / c[i];
+				m = m % c[i];
+				if (m % c[i] == 0)
+					break;
+			}
+		}
+		return (0);
+	}
+}
