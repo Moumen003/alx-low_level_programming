12a5a86df68ea57d2584841ac351bc9bb9c395a2
Moumen003 <moumenibrahim219@yahoo.com>
2023-09-01 16:53:53 +0000

    jhaha

---
+++ 0x09-static_libraries/m/5-strstr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+*_strstr - bs ab2a
+*@haystack: bs ba2a
+*@needle: we malo
+*
+*Return: 2ooly keda
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+int i;
+
+if (*needle == 0)
+	return (haystack);
+
+while (*haystack)
+{
+	i = 0;
+
+	if (haystack[i] == needle[i])
+	{
+		do {
+			if (needle[i + 1] == '\0')
+				return (haystack);
+			i++;
+		} while (haystack[i] == needle[i]);
+	}
+	haystack++;
+}
+return ('\0');
+}
