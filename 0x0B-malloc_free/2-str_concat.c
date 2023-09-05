2d93014a115db589dbb84136266d348f722b2f1a
“Moumen003” <moumenibrahim219@yahoo.com>
2023-09-05 23:58:58 +0000

    wemalo

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+* str_concat - fn name
+* @s1: input
+* @s2: another one
+* Return: kefyaa
+*/
+
+char *str_concat(char *s1, char *s2)
+{
+
+
+int size1, size2, size;
+char *ptr;
+
+for (size1 = 0; s1[size1] != '\0'; size1++)
+;
+
+for (size2 = 0; s2[size2] != '\0'; size2++)
+;
+
+size = size1 + size2;
+
+ptr = malloc(size *sizeof(*s1) + 1);
+
+if (ptr == 0)
+	return (0);
+
+while (size2--)
+{
+	ptr[size] = s2[size2];
+	size--;
+}
+while (size1--)
+{
+	ptr[size] = s1[size1];
+	size--;
+}
+
+return (ptr);
+}
