b5bb80cf0748c7a1fdd28aa6de648eb2cbc9b9c0
“Moumen003” <moumenibrahim219@yahoo.com>
2023-09-05 23:26:36 +0000

    wemalo

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -8,12 +8,13 @@
 
 char *_strdup(char *str)
 {
+	int size;
 	char *ptr = malloc(sizeof(str) + 1);
 
 	if (ptr == 0 || str == 0)
 		return (NULL);
 
-	for (; str[size] != '\0'; size++)
-		ptr[get] = str[get];
+	for (size = 0;; str[size] != '\0'; size++)
+		ptr[size] = str[size];
 	return (ptr);
 }
