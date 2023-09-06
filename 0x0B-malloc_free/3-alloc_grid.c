8d521d75a3a952cc6d7228f98eeca2806fd28f3f
“Moumen003” <moumenibrahim219@yahoo.com>
2023-09-06 00:36:12 +0000

    wemalo

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+* alloc_grid - fn name
+* @width: bos
+* @height: keda
+* Return: pointer to 2d
+*/
+
+int **alloc_grid(int width, int height)
+{
+
+	int i, j;
+
+	int **grid;
+
+	grid = malloc(sizeof(*grid) * height);
+
+	if (width <= 0 || height <= 0 || grid == 0)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(sizeof(**grid) * width)
+		if (grid[i] == 0)
+		{
+			while (i--)
+				free(grid[i]);
+			free(grid);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
+	}
+	return (grid);
+}
