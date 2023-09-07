dc138b3ef317cfa4c6462320fc6872ab93f96793
“Moumen003” <moumenibrahim219@yahoo.com>
2023-09-07 19:14:25 +0000

    bs ba2a

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,12 +1,29 @@
 #include "main.h"
 
 /**
+* _memset -bs ba2a
+* @s: we malo
+* @b: k
+* @n: bs
+* Return: abooooyAA
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *ptr = s;
+
+	while (n--)
+		*s++ = b;
+
+	return (ptr);
+}
+
+/**
 * _calloc - fn name is bery nice
 * @nmemb: bos
 * @size: shoof
 * Return: batee5
 */
-
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
@@ -19,7 +36,8 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (p == 0)
 		return (NULL);
-	while (sizeof(int) * nmemb)
-		*p++ = 0;
+
+	_memset(p, 0, sizeof(int) * nmemb);
+
 	return (p);
 }
