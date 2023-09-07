c8e04a5430559b015aac7996940afd9c74bdcdcf
“Moumen003” <moumenibrahim219@yahoo.com>
2023-09-07 19:17:46 +0000

    bs ba2a

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* array_range - fn name
+* @min: bs
+* @max: yabo
+* Retrun: 3amo
+*/
+
+int *array_range(int min, int max)
+{
+	int l, i;
+	int *ptr;
+
+	if (min < max)
+		return (NULL);
+	l = max - min + 1;
+	ptr = malloc(sizeof(int) * l);
+	if (!ptr)
+		return (NULL);
+	for (i = 0; i < l; i++)
+		ptr[i] = min++
+	return (ptr);
+}
