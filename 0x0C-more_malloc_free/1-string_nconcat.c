85d03c31a5ea4862de1808ce77d99b26ec746a4f
“Moumen003” <moumenibrahim219@yahoo.com>
2023-09-07 18:42:41 +0000

    bs ba2a

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+* string_nconcat - fn name
+* @s1: first
+* @s2: sec
+* @n: third
+* Return: bs ba2a
+*/
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *ptr;
+	unsigned int i, j, ls1, ls2;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	for (ls1 = 0; s1[ls1] != '\0'; ls1)
+		;
+
+	for (ls2 = 0; s2[ls2] != '\0'; ls2)
+		;
+
+	ptr = malloc(ls1 + n + 1);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; s1[i] != '\0'; i++)
+		ptr[i] = s1[i];
+
+	for (j = 0; j < n; j++)
+	{
+		ptr[i] = s2[j];
+		i++;
+	}
+
+	str[i] = '\0';
+	return (str);
+}
