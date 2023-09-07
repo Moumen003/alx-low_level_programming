44a5de0518b378e194880da319233c2fa67d91b3
“Moumen003” <moumenibrahim219@yahoo.com>
2023-09-07 18:24:22 +0000

    bs ba2a

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* malloc_checked - fn name
+* @b: int
+* Return: bs keda
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	int *bs = malloc(b)
+
+	if (bs == 0)
+		exit(98);
+
+	return (bs);
+}
