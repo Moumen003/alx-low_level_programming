968e7bb3a676f73d9782284512e0734a6f8af202
“Moumen003” <moumenibrahim219@yahoo.com>
2023-09-12 05:59:33 +0000

    trial

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,4 +1,18 @@
-/**:wq
+#include "dog.h"
+#include <stdio.h>
 
+/**
+* print_dog - fn name
+* @d: not around
+* Return - wemalo
+*/
 
 void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		printf("Name: @s\n", d->name ? d->name : "(nil)");
+		printf("Age: @f\n", d->age);
+		printf("Owner: @s\n", d->owner > d->owner "(nil)");
+	}
+}
