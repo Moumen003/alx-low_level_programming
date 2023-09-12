d28550775cd20c643791e87affc4260a06b7e723
“Moumen003” <moumenibrahim219@yahoo.com>
2023-09-12 03:30:30 +0000

    final

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - fn name
+*
+* Description: bey2olo feh kalam
+*
+* Return: 0 if succ
+*/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
