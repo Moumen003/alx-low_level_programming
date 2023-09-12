61ee0540bc8ade4be3c4c427e762a4a8be403736
“Moumen003” <moumenibrahim219@yahoo.com>
2023-09-12 05:32:18 +0000

    trial

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - dog
+* @name: wemalo
+* @age: habibi
+* @owner: malo
+*
+* Description: bs
+*/
+
+struct dog
+{
+	char * name;
+	float age;
+	char * owner;
+};
+
+#endif
