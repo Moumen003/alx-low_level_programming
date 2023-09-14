af8ea21cb15ca1b4648d13709443e3dc4e9f8054
“Moumen003” <moumenibrahim219@yahoo.com>
2023-09-14 23:17:41 +0000

    test

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,11 +13,13 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list args;
+	unsigned int i;
+
+	int s;
 
 	va_start(args, n);
-	unsigned int i, s;
 
-	S = 0;
+	s = 0;
 	for (i = 0; i < n; i++)
 	{
 		s += va_arg(args, int);
