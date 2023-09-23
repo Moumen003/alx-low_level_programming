1a11cf9225510139638dd84abd0eb9f07fec6778
“Moumen003” <moumenibrahim219@yahoo.com>
2023-09-23 00:14:50 +0000

    test

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+
+/**
+* _strlen - fn name
+* @s: string
+*
+* Return: length
+*/
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
+* print_list - fn name
+* @h: name
+*
+* Return: size
+*/
+
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)";
+		h = h->next;
+		i++
+	}
+	return (i);
+}
