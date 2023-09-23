bc8d3de57ba5c7c52e96618357414228ee9a56d5
“Moumen003” <moumenibrahim219@yahoo.com>
2023-09-23 00:54:35 +0000

    test

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+* free_list - fn name
+* @head: startingpt
+*
+*  Return: void
+*/
+
+void free_list(list_t *head)
+{
+	list_t *node, *next;
+
+	if (!head)
+		return;
+
+	node = head;
+	while(node)
+	{
+		next = node->next;
+		free(node->str);
+		free(node);
+		node = next;
+	}
+}
