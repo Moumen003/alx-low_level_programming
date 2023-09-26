9875f468b24ce387aa4ce487ea303bec4cfb21b7
“Moumen003” <moumenibrahim219@yahoo.com>
2023-09-26 15:32:15 +0000

    test

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+* insert_nodeint_at_index - fn name
+* @head: ptr to head
+* @idx: wanted index
+* @n: data
+*
+* Return: nth needed
+*/
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *node, *new;
+	unsigned int i;
+
+	new = malloc(sizeof(listint_t));
+	if (!new || !head)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+
+	if (!idx)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+
+	for (i = 0, node = *head, node, i < idx; i++)
+	{
+		node = node->next;
+		if (i == idx - 1)
+		{
+			new->next = node->next;
+			node->next = new;
+		{
+	}
+	return (new);
+}
