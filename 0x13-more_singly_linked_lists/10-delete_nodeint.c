c8a22eb2b044e86564a5c4ede148d25711c282d0
“Moumen003” <moumenibrahim219@yahoo.com>
2023-09-26 15:51:42 +0000

    test

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -32,7 +32,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 			prev->next = node->next;
 			free(node);
 			return (1);
-		{
+		}
 		i++;
 		prev = node;
 		node = node->next;
