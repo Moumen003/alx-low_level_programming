47d3e506d19946b62a456d1353810b2540d39a25
“Moumen003” <moumenibrahim219@yahoo.com>
2023-09-26 16:32:37 +0000

    test

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,41 +1,40 @@
-#include "lists.h"
+nclude "lists.h"
 
 /**
-* delete_nodeint_at_index - fn name
-* @head: enta 3aref
-* @index: el feha
-*
-* Return: 1 if success
-*/
-
+ * delete_nodeint_at_index - deletes a node in a linked list at a certain index
+ * @head: pointer to the first element in the list
+ * @index: index of the node to delete
+ *
+ * Return: 1 (Success), or -1 (Fail)
+ */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	unsigned int i;
-	listint_t *node, *prev;
+	listint_t *temp = *head;
+	listint_t *current = NULL;
+	unsigned int i = 0;
 
-	if (!head || !*head)
+	if (*head == NULL)
 		return (-1);
 
-	if (!index)
+	if (index == 0)
 	{
-		node = *head;
 		*head = (*head)->next;
-		free(node);
+		free(temp);
 		return (1);
 	}
 
-	node = *head;
-	while (node)
+	while (i < index - 1)
 	{
-		if (i == index)
-		{
-			prev->next = node->next;
-			free(node);
-			return (1);
-		}
+		if (!temp || !(temp->next))
+			return (-1);
+		temp = temp->next;
 		i++;
-		prev = node;
-		node = node->next;
 	}
-	return (-1);
+
+
+	current = temp->next;
+	temp->next = current->next;
+	free(current);
+
+	return (1);
 }
