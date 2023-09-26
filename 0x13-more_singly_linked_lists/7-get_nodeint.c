da5c400711d00b7cf15f49284df52bc15a824f02
“Moumen003” <moumenibrahim219@yahoo.com>
2023-09-26 15:05:47 +0000

    test

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+* get_nodeint_at_index - fn name
+* @head: first
+* @index: wemallo
+*
+* Return: dont know
+*/
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	size_t i;
+
+	for (i = 0; i < index; i++)
+	{
+		head = head->next;
+		if (head->next == NULL && i < index)
+			return (NULL);
+	}
+	return (head);
+}
