636dc206e688d56a6850deb44797d737e80874be
“Moumen003” <moumenibrahim219@yahoo.com>
2023-09-28 10:33:46 +0000

    test

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary equivalent of a decimal number
+ * @n: number to print in binary
+ */
+
+void print_binary(unsigned long int n)
+{
+	int b = sizeof(n) * 8, p = 0;
+
+	while (b)
+	{
+		if (n & 1L << --bit)
+		{
+			_putchar('1');
+			p++;
+		}
+		else if (p)
+			_putchar('0');
+	}
+	if (!p)
+		_putchar('0');
+}
