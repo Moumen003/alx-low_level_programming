4ccddc79fb8a94cd524a8c4b3fad8c5f4bf0f3df
“Moumen003” <moumenibrahim219@yahoo.com>
2023-09-28 10:05:26 +0000

    test

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* binary_to_uint - fn name
+* @b: input binary
+*
+* Return: unsigned int
+*/
+
+unsigned int binary_to_uint(const char *b)
+{
+	int i = 0;
+	unsigned int sum = 0;
+
+	for (; b[i] != '\0'; i++)
+	{
+		if (b[i] != 1 && b[i] != 0)
+			return (NULL);
+		sum = sum + b[i] * 2 ^ (7 - i);
+	}
+	return (sum);
+}
