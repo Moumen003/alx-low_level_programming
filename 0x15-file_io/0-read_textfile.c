4e65550d64687fa5f8eb61c7ba3a24f818de9bce
“Moumen003” <moumenibrahim219@yahoo.com>
2023-10-02 17:57:38 +0000

    bs ba2a

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile- Read text file print to STDOUT.
+ * @filename: text file being read
+ * @letters: number of letters to be read
+ * Return: w- actual number of bytes read and printed
+ *        0 when function fails or filename is NULL.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fp = fopen(filename, O_RDONLY);
+	char *buf = malloc(sizeof(char) * letters;
+	ssize_t bytes;
+
+	if (fp == -1)
+		return (0);
+	if (!filename || !letters)
+		return (0);
+
+	bytes = read(fp, , letters);
+	bytes = write(STDOUT_FILENO, , bytes);
+	close(fp);
+	return (bytes);
+}
