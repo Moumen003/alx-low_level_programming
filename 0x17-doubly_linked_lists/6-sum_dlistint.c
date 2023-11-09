da8294c33b9c00d1cbf8f300a7e5b67226dacd2e
Moumen003 <moumenibrahim219@yahoo.com>
2023-11-09 18:19:22 +0200

    wemalo

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -9,12 +9,10 @@
 int sum_dlistint(dlistint_t *head)
 {
 	int sum = 0;
+	dlistint_t *i;
 
-	while (head)
-	{
-		sum += head->n;
-		head = head->next;
-	}
+	for (i = head; i != NULL; i = i->next)
+		sum += i->n;
 
 	return (sum);
 }
