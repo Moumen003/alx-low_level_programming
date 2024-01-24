5a47ddaa53fa48743ebb2151fc358339988786dd
Moumen003 <moumenibrahim219@yahoo.com>
2024-01-25 00:36:02 +0200

    what

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,8 +7,7 @@
  * Return: If an error occurs - NULL.
  */
 
-hash_table_t *hash_table_create(unsigned long int size)
-{
+hash_table_t *hash_table_create(unsigned long int size){
 	hash_table_t *ht;
 	unsigned long int i;
 
